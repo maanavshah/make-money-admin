1d07ea065537552c9ff1b395345b71a16ce437bd
maanav <shah.maanav.07@gmail.com>
2019-12-21 02:19:20 +0530

    Add devise config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'makemoneyadmin.herokuapp.com', port: 3000 }
 end
