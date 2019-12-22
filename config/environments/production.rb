5e2658f66caeeff2665162b61cfa800e1a063017
maanav <shah.maanav.07@gmail.com>
2019-12-23 01:38:21 +0530

    Important fix for user email confirmation to domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,14 +90,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'makemoneyadmin.herokuapp.com', port: '3000' }
+  config.action_mailer.default_url_options = { host: 'makemoneyadmin.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'makemoneyadmin.herokuapp.com:3000',
+    domain: 'makemoneyadmin.herokuapp.com',
     user_name: 'shah.maanav.07@gmail.com',
     password: 'Vardhamanswami7304351982',
     authentication: :plain,
